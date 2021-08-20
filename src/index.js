222e04f335640f1e3f6eaf32bfe04c91df474d25
Artyom Khrystaforau <Xisera@mail.ru>
2021-08-20 04:22:41 +0300

    feat: rewrite solution

--- src/index.js
+++ src/index.js
@@ -1,40 +1,40 @@
-
 exports.min = function min (array) {
     if(!array || array.length === 0){
-        return 0;
+      return 0;
+    }
+  
+    let result = array[0];
+    for(let i = 1; i < array.length; i++){
+      if(result > array[i]){
+        result = array[i];
       }
-  let minimum = array[0];
-  for(let i = 1; i < array.length; i++ ) {
-      if(array[i] < minimum) {
-          minimum = array[i];
-      }
-      i++
+    }
+    return result;
   }
-  return minimum;
-}
-
-exports.max = function max (array) {
+  
+  exports.max = function max (array) {
     if(!array || array.length === 0){
-        return 0;
-      }
-    let maximum = array[0];
-    for(let i = 1; i < array.length; i++ ) {
-        if(array[i] > maximum) {
-            maximum = array[i];
-        }
-        i++
+      return 0;
     }
-    return maximum;
-}
-
-exports.avg = function avg (array) {
+  
+    let result = array[0];
+    for(let i = 1; i < array.length; i++){
+      if(result < array[i]){
+        result = array[i];
+      }
+    }
+    return result;
+  }
+  
+  exports.avg = function avg (array) {
     if(!array){
-        return 0;
-      }
-    let sum = 0, counter = 0;
-    for(let i = 0; i < array.length; i++) {
-        sum += array[i];
-        counter++;
+      return 0;
     }
-    return count > 0 ? sum / counter : 0;
-}
+    let result = 0;
+    let count = 0;
+    for (let i = 0; i < array.length; i++){
+        result += array[i];
+        count++;
+    }
+    return count > 0 ? result / count : 0;
+  }
